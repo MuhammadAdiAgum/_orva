eba8a6ccd18a0b1836454115a57c1697eece0c1b
MuhammadAdiAgum <adiagum4.1@gmail.com>
2023-06-12 22:42:17 +0800

    orva

--- log_dashboard/ajax/log.php
+++ log_dashboard/ajax/log.php
@@ -15,44 +15,23 @@ $log_count = count($log_result);
 
 ?>
 
-<div class="card-header bg-dark text-center text-white" style="letter-spacing: 2px; font-size: 17px;">
+<div class="card-header bg-dark text-start text-white" style="letter-spacing: 2px; font-size: 17px;">
     <strong>
-        ANOMALOUS ACTIVITY
+        ATTACK ATTEMPT RECORD
     </strong>
-
 </div>
-<div class="card-body m-0 p-0" style="max-height: 477px; overflow-y: scroll;">
+<div class="card-body m-0 p-0" style="max-height: 450px; overflow-y: scroll;">
     <div class="accordion ">
-
         <?php
         $x = 0;
         foreach ($log_result as $log_list) {
 
-            // orva-v3
-            // $date = date('d F Y', strtotime($log_list['2'])); #. shell_exec("date +'%Y'");
-            // $time = date('H:i:s ', strtotime($log_list['3'] . ':' . $log_list['4'] . ':' . $log_list['5']));
-            // $protocol = preg_replace("/[^a-zA-Z0-9]/", "", $log_list['13']);
-            // $target_data_center = $log_list['0'] . '-' . $log_list['1'];
-            // $target_server = $log_list['7'];
-            // $target_ip_address = $log_list['16'];
-            // $target_port = $log_list['17'];
-            // $attacker_ip_address = $log_list['14'];
-            // $attacker_id = $log_list['15'];
-            // $attacker_method = $log_list['8'];
-            //==========================================================================
             // orva-v4
             $v4_id = $log_list['3'];
 
             $origin_attack_timestamp_format = 'm/d/y-H:i:s.u';
             $origin_attack_timestamp = $log_list['4'];
             // $convert_date = DateTimeImmutable::createFromFormat($origin_attack_timestamp_format, $origin_attack_timestamp);
-
-
-
-
-            // var_dump($v4_attack_date);
-            // $v4_attack_date =  $convert_date->format('d/m/Y');
-            // $v4_attack_time =  $convert_date->format('H:i:s');
 
             $v4_rsyslog_date = date('d/m/Y', strtotime($log_list['0'])); #. shell_exec("date +'%Y'");
             $v4_rsyslog_time = date('H:i:s ', strtotime($log_list['1']));
@@ -80,15 +59,14 @@ $log_count = count($log_result);
             $v4_attacker_mac_address = $log_list['14'];
             $v4_attacker_ip_address = $log_list['6'];
             $v4_attacker_port = $log_list['7'];
-            //==========================================================================
-            // accordion
+            //==== accordion ===========================================================
             $id = 'id-' . $x;
             $x++;
             //==========================================================================
             if ($log_list['6'] == null && $log_list['7'] == null) {
                 $log_count = 0; ?>
-                <div class="accordion-item border-0 rounded-0 align-middle text-center p-3">
-                    <span class="">No Activities</span>
+                <div class="accordion-item border-0 rounded-0 d-flex align-items-center justify-content-center p-3">
+                    <span style="letter-spacing: 1px;" class=""><b>No Activities.</b></span>
                 </div>
             <?php
             } else {
@@ -97,12 +75,17 @@ $log_count = count($log_result);
             ?>
                 <div class="accordion-item rounded-0">
                     <div class="accordion-button border-0 rounded-0 collapsed pt-2 pb-2" data-bs-toggle="collapse" data-bs-target="#<?= $id; ?>">
-                        <table width="100%" class="table table-borderless p-0 m-0">
+                        <table width="100%" class=" table table-borderless p-0 m-0">
                             <tr>
-                                <td width="30%" class="ps-0">ID : <strong><?= $v4_id; ?></strong></td>
-                                <td width="40%" class="ps-0">Type : <strong><?= $v4_type; ?></strong></td>
-                                <td width="30%" class="ps-0">Time : <strong><?= $v4_attack_date; ?></strong> <strong><?= $v4_attack_time; ?></strong></td>
-                                <!-- <td width="20%" class="ps-0">Time : <strong><?= $v4_time; ?></strong></td> -->
+                                <td width="55%" class="ps-0">
+                                    Alert : <strong><?= $v4_type; ?></strong> <i>in</i> <strong><?= $v4_target_data_center; ?></strong>
+                                </td>
+                                <td width="30" class="ps-0">
+                                    Time : <strong><?= $v4_attack_date; ?></strong> <strong><?= $v4_attack_time; ?></strong>
+                                </td>
+                                <td width="15%" class="ps-0">
+                                    ID : <strong><?= $v4_id; ?></strong>
+                                </td>
                             </tr>
                         </table>
                     </div>
@@ -186,7 +169,6 @@ $log_count = count($log_result);
                                     </div>
                                 </div>
                             </div>
-
                             <div class="row">
                                 <div class="col-lg-6">
                                     <div class="my-border mb-3">
@@ -235,8 +217,6 @@ $log_count = count($log_result);
                                     </div>
                                 </div>
                             </div>
-
-
                         </div>
                     </div>
                 </div>
@@ -244,8 +224,11 @@ $log_count = count($log_result);
         <?php } ?>
     </div>
 </div>
-<div class="card-footer bg-dark text-end text-white">
-    <span class="text-muted">Log Count : </span>
-    <strong><?= $log_count; ?></strong>
-</div>
+<div class="card-footer bg-dark">
+    <div class="row">
+        <div class="text-end text-white">
+            <span class="text-muted">Log Count : </span>
+            <strong class=""><?= $log_count; ?></strong>
+        </div>
+    </div>
 </div>
